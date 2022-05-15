bd16ee99258237b9e6fcb6a8950b58d41069081b
BEANS <ethan@walkersoft.com.au>
2022-05-15 12:34:05 +1000

    werthj

--- Children-Multi.py
+++ Children-Multi.py
@@ -16,17 +16,17 @@ if int(age) > 20:
    print("Welcome To The Parent Control Panel!")
    print("You Can Control Your Child's Multiplication Quiz")
    print("Mode = " + str(hard_mode))
-   hard_mode_proxy = input("Hello what would you like to set the mode to " + name)
+   hard_mode_proxy = input("Hello what would you like to set the mode to " + name + " ")
    bool(hard_mode_proxy)
    hard_mode = hard_mode_proxy
    print("hard mode = " + hard_mode)
    
-print("What is 4 x 5? ")
+print("What is 10/2? ")
 print("(a)     (b)")
-print("32      20 ")
+print("9      5 ")
 print("")
 Test = input()
-if Test == "b" or Test == "20":
+if Test == "b" or Test == "5":
     print("Congratulations You Are Correct!")
     print("You Will Be Awarded 50exp")
     xp = xp + 50
@@ -35,21 +35,21 @@ else:
     print("Incorrect You must now go to the tutorial!")
     print("Welcome To The Tutorial!")
     print("")
-    print("Multiplication is easy all you need to do is add")
-    print("that number the amount of times e.g 2x3/2+2+2 as")
-    print("you can see i added 2, 3 times!This is what makes")
+    print("Disvision is easy all you need to do is Split")
+    print("that number the amount of times e.g 2/8=4+4 as")
+    print("you can see i di 2, 3 times!This is what makes")
     print("it so easy!")
     print("")
     input("Type anything to proceed: ")
 
 
-print("what is 3x6?")
+print("what is 9/3?")
 print("(a)     (b)")
-print("18      13 ")
+print("7      3 ")
 print("")
 
 Test = input()
-if Test == "a" or "18":
+if Test == "b" or "3":
     print("Congratulations You Are Correct!")
     print("You Will Be Awarded 50exp")
     xp = xp + 50
@@ -59,26 +59,7 @@ else:
     print("Your exp is now" + xp)
 input("Type anything to procced")
 if hard_mode == True:
-    print("Welcome To The hard mode bonus quistion!")
-    print("")
-    print("What is 287 x 9?")
-    print("2583    2467")
-    print(" a       b")
-    print("")
-    print(" c       d ")
-    print("2896    2479")
-    hard_quiz1 = input()
-
-if hard_quiz == "a" or "2583":
-        print("congratulations you are correct!")
-        print("You will now be given 150exp")
-        xp = xp + 150
-        print("Your xp is now" + str(xp))
-if hard_quiz == "b" or "c" or "d" or "2467" or "2896" or "2479":
-  print("You are incorrect")
-  print("but we will give you an extra 25exp!")
-  xp = xp + 25
-  print("Your exp is now" + str(xp))
+    print("Sorry Hard Mode Is Disabled For Now!")
 
 print("Thank you for using our repo! #alpha")
 print("DO NOT DEL OR ELSE")
